2b4cdb348279825e5d25aeb736aee666869fb69b
Ubuntu <tgoerner@gmail.com>
2013-07-23 11:04:15 +0000

    final primes program with outfile

--- primes.js
+++ primes.js
@@ -1,22 +1,25 @@
-var primes = [1];
+#!/usr/bin/env node 
+var fs = require('fs');
+var outfile = "primes.txt";
+
+
 
 // Loop until generated the first 100 primes
-// length of prims is 100
-//for (var length=primes.length; length < 100; i++) {
+var primes = [];
 checki: 
-for (var i = 3; i <= 60; i++) {
+for (var i = 2; primes.length <= 100; i++) {
 	checkn: 
 	for (var n = i - 1; n >= 2; n--){
-		console.log("\**Testing i=" + i + " divding by n="+n);
+		// console.log("\**Testing i=" + i + " divding by n="+n);
 		result = i / n;
 		ceil = Math.ceil(result);
 		floor = Math.floor(result);
 		if (ceil == floor) {
-			console.log("not a prime: "+ i + " is divisible by " + n);
+			// console.log("not a prime: "+ i + " is divisible by " + n);
 			continue checki;
 		}
 		else if (n > 2) {
-			console.log("could be a prime, " + i + " does not divide cleanly by " + n + " and results in " + result);
+			// console.log("could be a prime, " + i + " does not divide cleanly by " + n + " and results in " + result);
 			continue checkn;
 		}
 		else if (n = 2) {
@@ -30,4 +33,5 @@ for (var i = 3; i <= 60; i++) {
 	}
 }
 
-console.log(primes);
+fs.writeFileSync(outfile, primes); 
+console.log("Script: " + __filename + "\nWrote: " + primes + "\nTo: " + outfile);
